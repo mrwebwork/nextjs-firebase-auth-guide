25eebe349ecce871f84265b1329e5961a9c6bb62
mrwebwork <tech@allanswebwork.info>
2023-07-11 22:43:05 -0500

    Add Toaster component to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,23 @@
 import "./globals.css";
+
 import { Montserrat } from "next/font/google";
+
+import { Toaster } from "react-hot-toast";
 
 const mont = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Firebase Next.js Auth",
-  description: "Firebase & Next.js Authentication Tutorial",
+  title: "Next.js & Firebase Auth",
+  description: " Next.js & Firebase Authentication Tutorial",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={mont.className}>{children}</body>
+      <body className={mont.className}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
